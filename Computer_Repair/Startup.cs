7a7a166080209cf69fb2bc59d81f8c00600ee153
Vlad <Vlad@VLAD->
2016-09-21 17:16:27 +0300

    Добавьте файлы проекта.

---
+++ Computer_Repair/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Computer_Repair.Startup))]
+namespace Computer_Repair
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
